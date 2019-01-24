679fe347ce2df54aae7961481edaa83d15b2777b
designMoreWeb <divystolia@gmail.com>
2019-01-24 18:28:31 -0500

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+class App extends Component{
+    state={
+        showBalloon:false,
+    };
 
+    toggle= () =>{
+        this.setState(prevState =>({
+            showBalloon: !prevState.showBalloon,
+        }));
+    };
+    
 ReactDOM.render(<App />,document.querySelector('#root'));
